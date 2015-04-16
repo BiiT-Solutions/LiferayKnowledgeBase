37b050fe0a19755c6372bd8cc65c4b1603b9fec4
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-04-16 17:21:32 +0200

    Version updated of liferay-commons

--- src/main/java/com/biit/liferay/access/exceptions/ArticleNotDeletedException.java
+++ src/main/java/com/biit/liferay/access/exceptions/ArticleNotDeletedException.java
@@ -1,6 +1,7 @@
 package com.biit.liferay.access.exceptions;
 
 public class ArticleNotDeletedException extends Exception {
+	private static final long serialVersionUID = 5514879587695238968L;
 
 	public ArticleNotDeletedException(String message) {
 		super(message);
