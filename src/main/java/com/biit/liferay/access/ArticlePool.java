8021d874fea17040fd53f5c4317a91fb4d4b88c3
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-11-03 10:41:52 +0100

    Pool duration in settings added.

--- src/main/java/com/biit/liferay/access/ArticlePool.java
+++ src/main/java/com/biit/liferay/access/ArticlePool.java
@@ -1,16 +1,10 @@
 package com.biit.liferay.access;
 
-import java.util.Enumeration;
-import java.util.Hashtable;
-
+import com.biit.liferay.access.config.KnowledgeBaseConfigurationReader;
 import com.biit.liferay.model.IArticle;
+import com.biit.utils.pool.SimplePool;
 
-public class ArticlePool {
-
-	protected final static long EXPIRATION_TIME = 3600000;// 60 minutes
-
-	private Hashtable<Long, Long> insertionTime; // element id --> time
-	private Hashtable<Long, IArticle<Long>> articles; // Roles by user.
+public class ArticlePool extends SimplePool<Long, IArticle<Long>> {
 
 	private static ArticlePool instance = new ArticlePool();
 
@@ -18,46 +12,14 @@ public class ArticlePool {
 		return instance;
 	}
 
-	private ArticlePool() {
-		reset();
+	@Override
+	public boolean isDirty(IArticle<Long> element) {
+		return false;
 	}
 
-	public void reset() {
-		insertionTime = new Hashtable<Long, Long>();
-		articles = new Hashtable<Long, IArticle<Long>>();
-	}
-
-	public void addArticle(IArticle<Long> article) {
-		if (article != null) {
-			insertionTime.put(article.getId(), System.currentTimeMillis());
-			articles.put(article.getId(), article);
-		}
-	}
-
-	public void removeArticle(long kbArticleId) {
-		insertionTime.remove(kbArticleId);
-		articles.remove(kbArticleId);
-	}
-
-	public IArticle<Long> getArticleByResourceKey(long resourceKey) {
-		long now = System.currentTimeMillis();
-		Long kbArticleId = null;
-		if (insertionTime.size() > 0) {
-			Enumeration<Long> e = insertionTime.keys();
-			while (e.hasMoreElements()) {
-				kbArticleId = e.nextElement();
-				if ((now - insertionTime.get(kbArticleId)) > EXPIRATION_TIME) {
-					// object has expired
-					removeArticle(kbArticleId);
-					kbArticleId = null;
-				} else {
-					if (articles.get(kbArticleId).getResourcePrimKey().equals(resourceKey)) {
-						return articles.get(kbArticleId);
-					}
-				}
-			}
-		}
-		return null;
+	@Override
+	public long getExpirationTime() {
+		return KnowledgeBaseConfigurationReader.getInstance().getArticlePoolExpirationTime();
 	}
 
 }
---
+++ src/main/java/com/biit/liferay/access/config/KnowledgeBaseConfigurationReader.java
@@ -0,0 +1,59 @@
+package com.biit.liferay.access.config;
+
+import com.biit.usermanager.entity.pool.config.PoolConfigurationReader;
+import com.biit.utils.configuration.ConfigurationReader;
+import com.biit.utils.configuration.PropertiesSourceFile;
+import com.biit.utils.configuration.SystemVariablePropertiesSourceFile;
+import com.biit.utils.configuration.exceptions.PropertyNotFoundException;
+
+public class KnowledgeBaseConfigurationReader extends ConfigurationReader {
+	private static final String CONFIG_FILE = "settings.conf";
+	private static final String SYSTEM_VARIABLE_CONFIG = "USER_MANAGER_CONFIG";
+
+	// Tags
+	private static final String ARTICLE_EXPIRATION_TIME = "knowledgebase.article.expiration";
+
+	// Default
+	private static final String DEFAULT_EXPIRATION_TIME = "300000";
+
+	private static KnowledgeBaseConfigurationReader instance;
+
+	private KnowledgeBaseConfigurationReader() {
+		super();
+
+		addProperty(ARTICLE_EXPIRATION_TIME, DEFAULT_EXPIRATION_TIME);
+
+		addPropertiesSource(new PropertiesSourceFile(CONFIG_FILE));
+		addPropertiesSource(new SystemVariablePropertiesSourceFile(SYSTEM_VARIABLE_CONFIG, CONFIG_FILE));
+
+		readConfigurations();
+	}
+
+	public static KnowledgeBaseConfigurationReader getInstance() {
+		if (instance == null) {
+			synchronized (PoolConfigurationReader.class) {
+				if (instance == null) {
+					instance = new KnowledgeBaseConfigurationReader();
+				}
+			}
+		}
+		return instance;
+	}
+
+	private String getPropertyLogException(String propertyId) {
+		try {
+			return getProperty(propertyId);
+		} catch (PropertyNotFoundException e) {
+			return null;
+		}
+	}
+
+	public Long getArticlePoolExpirationTime() {
+		try {
+			return Long.parseLong(getPropertyLogException(ARTICLE_EXPIRATION_TIME));
+		} catch (Exception e) {
+			return Long.parseLong(DEFAULT_EXPIRATION_TIME);
+		}
+	}
+
+}
