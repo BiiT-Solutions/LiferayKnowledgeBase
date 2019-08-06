5b24e205abcffe3e5bf3d84a678589aec9208832
Jorge Hortelano <jhortelano@biit-solutions.com>
2019-08-06 13:42:48 +0200

    Changed log levels

--- src/main/java/com/biit/liferay/access/ArticleService.java
+++ src/main/java/com/biit/liferay/access/ArticleService.java
@@ -444,11 +444,11 @@ public class ArticleService extends ServiceAccess<IArticle<Long>, KbArticle> imp
 		params.add(new BasicNameValuePair("parentResourcePrimKey", Long.toString(folderId)));
 		params.add(new BasicNameValuePair("priority", "1.0"));
 
-		LiferayClientLogger.warning(this.getClass().getName(), "Moving article with params " + params);
+		LiferayClientLogger.debug(this.getClass().getName(), "Moving article with params " + params);
 		String result = getHttpResponse("knowledge-base-portlet.kbarticle/move-kb-article", params);
 
 		// A Simple JSON Response Read
-		LiferayClientLogger.warning(this.getClass().getName(),
+		LiferayClientLogger.info(this.getClass().getName(),
 				"Moving article '" + resourcePrimKey + "' to folder  '" + folderId + "' has as result '" + result + "'.");
 	}
 
