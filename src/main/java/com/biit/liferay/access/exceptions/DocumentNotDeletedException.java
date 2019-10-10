69cfdb916fb3e85995c7f285ee7eba38f219eca7
Jorge Hortelano <jhortelano@biit-solutions.com>
2019-10-10 11:41:50 +0200

    Added missing serial number

--- src/main/java/com/biit/liferay/access/exceptions/DocumentNotDeletedException.java
+++ src/main/java/com/biit/liferay/access/exceptions/DocumentNotDeletedException.java
@@ -1,6 +1,7 @@
 package com.biit.liferay.access.exceptions;
 
 public class DocumentNotDeletedException extends Exception {
+	private static final long serialVersionUID = -373227480739955408L;
 
 	public DocumentNotDeletedException(String text) {
 		super(text);
