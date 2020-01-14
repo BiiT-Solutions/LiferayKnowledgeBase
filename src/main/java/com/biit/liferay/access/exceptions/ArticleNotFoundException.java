8c7315fa17e09c47045482c34f17ad331045ea00
Jorge Hortelano <jhortelano@biit-solutions.com>
2020-01-14 11:38:01 +0100

    Fixed test

---
+++ src/main/java/com/biit/liferay/access/exceptions/ArticleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.biit.liferay.access.exceptions;
+
+public class ArticleNotFoundException extends Exception {
+	private static final long serialVersionUID = -9150758065570808381L;
+
+	public ArticleNotFoundException(String message) {
+		super(message);
+	}
+}
